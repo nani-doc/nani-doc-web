b2df20c7f83ddf2ab787a175bb79de6d56afba8e
lmm1990 <544484078@qq.com>
2022-02-16 10:16:09 +0800

    调整配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import { BASE_PATH } from './src/constant'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nani-doc-web/',
+  base: `${BASE_PATH}/`,
   plugins: [vue()],
   server: {
     host: '0.0.0.0'
